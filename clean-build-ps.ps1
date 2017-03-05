8ac55a98713024b1592a0360a67ceb8dad8fa18c
Adrian Tudorache <a.p.tudorache@gmail.com>
2017-03-05 16:28:04 +0100

    clean-build improved

--- clean-build-ps.ps1
+++ clean-build-ps.ps1
@@ -1,5 +1,12 @@
 # Get all bin and obj folders and force delete them
+Write-Output "Remove the bin and obj folders"
 Get-ChildItem -Include bin,obj -Recurse | Remove-Item -Recurse -Force
+
+Write-Output "Remove the node_modules folders"
+Get-ChildItem -Include node_modules,dist -Recurse | Remove-Item -Recurse -Force
+
+Write-Output "Remove the packages folder"
+Get-ChildItem -Include packages -Recurse -exclude repositories.config | Remove-Item -Recurse -Force
 
 Exit
 
